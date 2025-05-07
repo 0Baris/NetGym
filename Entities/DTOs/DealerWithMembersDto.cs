cb30548d0f1e6bf34dce5b72452cdc1a00e5486e
Barış Cem Ant <baris.ant@ogr.gelisim.edu.tr>
2025-05-08 01:29:20 +0300

    Controller sınıfları ve DTOlar eklendi, Manager ve Servisler düzenlendi.

---
+++ Entities/DTOs/DealerWithMembersDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class DealerWithMembers : IDto
+    {
+        public int DealerId { get; set; }
+        public string DealerName { get; set; }
+        public string DealerPhoneNumber { get; set; }
+        public string DealerEmail { get; set; }
+        public string DealerAddress { get; set; }
+        public string MemberName { get; set; }
+        public string MemberPhoneNumber { get; set; }
+        public string MemberEmail { get; set; }
+        public string MemberIdentityNumber { get; set; }
+        public DateTime MemberBirthDate { get; set; }
+        public DateTime MemberRegistrationDate { get; set; }
+    }
+}
