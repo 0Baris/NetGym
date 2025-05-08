03699349a9d49f73b335de09ed21a839b228bc83
Barış Cem Ant <baris.ant@ogr.gelisim.edu.tr>
2025-05-09 00:32:43 +0300

    Memory Cache eklendi.

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
